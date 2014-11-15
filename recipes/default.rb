27da2dbc989322bc63ec05b40c613bf55e1615c6
Pradeep Mishra <unixworld@users.noreply.github.com>
2014-11-15 12:41:05 +0530

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -10,29 +10,3 @@ chef_gem "aws-sdk" do
 end
 
 require 'aws-sdk'
-
-# Create Volume with snapshot id
-# aws_volume_ebs_volume "db_ebs_volume" do
-# 	size 5
-# 	device "/dev/sdb"
-# 	action [ :create, :attach ]
-# end
-
-# # Create new Volume
- # aws_volume_ebs_volume "db_ebs_volume" do
- #  	size 1
- # 	device "/dev/sde"
- #  	action [ :create, :attach ]
- # end
-
-# Attach a volume
-# aws_volume_ebs_volume "db_ebs_volume" do
-#  	volume_id "vol-d6af1dd3"
-#  	device "/dev/sdb"
-#  	action [ :attach ]
-# end
-
-# aws_volume_ebs_volume "db_ebs_volume" do
-# 	data_bag [ "EC2", "key" ]
-#  	action [ :test ]
-# end
