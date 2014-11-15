4b0aaaa3592daae013e5b9e167156007d63e5efe
Pradeep Mishra <unixworld@users.noreply.github.com>
2014-11-15 12:35:25 +0530

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,6 +3,5 @@
 # Attributes:: default
 #
 
-
 default['aws_volume']['aws_sdk_version'] = "1.45.0"
-default['chef']['secretfile'] = "/etc/chef/secret_key"
+default['chef']['secretfile'] = "/etc/chef/secret_key"  # path to secertfile.
