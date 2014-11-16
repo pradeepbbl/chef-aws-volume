e50afb5a8b85c7c18df1f6372e8f9649a14e5d46
Pradeep Mishra <unixworld@users.noreply.github.com>
2014-11-16 14:44:08 +0530

    Fix of #1 and #2
    Upgrade the version from 0.1.1 to 0.1.2 with two fixes #1 and #2

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email  "unixworld88@gmail.com"
 license 			    "Apache 2.0"
 description 		  "Managing AWS ebs resources"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.1.1"
+version           "0.1.2"
 recipe 			      "aws", "Installs the aws-sdk gem during compile time"
