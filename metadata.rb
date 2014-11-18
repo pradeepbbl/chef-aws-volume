5fed3ff0ac221bce26a4b3ac4890f383ff9329b3
Pradeep Mishra <unixworld88@gmail.com>
2014-11-18 20:47:14 +0530

    Revert the  test commit [issue #4]

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email  "unixworld88@gmail.com"
 license 			    "Apache 2.0"
 description 		  "Managing AWS ebs resources"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.1.4"
+version           "0.1.3"
 recipe 			      "aws", "Installs the aws-sdk gem during compile time"
